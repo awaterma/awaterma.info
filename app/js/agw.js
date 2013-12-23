15a0c1efa43e26229e5a2ead1325d7a07736a594
Andrew Waterman <awaterma@ecosur.mx>
2013-12-22 17:40:06 -0800

    Changes to clean-up js. Minor editorial updates. Fixes.

--- app/js/agw.js
+++ app/js/agw.js
@@ -1,58 +1,47 @@
 /**
  * Copyright 2013, Andrew Glenn Waterman
  */
-
 jQuery(document).ready(function () {
         "use strict";
-        
-        function hide(divs) {
-			for (var counter in divs) {
-               jQuery(divs[counter]).hide();
-			}
+        function hideAll() {
+          var hide = function hide(divs) {
+            for (var counter in divs) {
+              jQuery(divs[counter]).hide();
+            }
             jQuery('#nav').children().each(function () {
-               jQuery(this).removeClass('active');
+              jQuery(this).removeClass('active');
             });
+          };
+          hide(['#about','#opensource','#stream','#writing']);
         }
 
-		function hideAll() {
-			hide(hide(['#about','#opensource','#stream','#writing']));
-		}
-
+        function showPanel(panel, tag, breadCrumb) {
+          hideAll();
+          jQuery(panel).fadeIn('medium');
+          jQuery(tag).parent().addClass('active');
+          jQuery('.breadcrumb').children().last().text(breadCrumb);
+        }
         /* Bind controls */
         jQuery('#back').click(function(){
-            hideAll();
-            jQuery('#about').fadeIn('medium');
-            jQuery('#back').parent().addClass('active');
-            jQuery('.breadcrumb').children().last().text("background");
+          showPanel('#about','#back','me');
         });
 
         jQuery('#open').click(function() {
-            hideAll();
-            jQuery('#opensource').fadeIn('medium');
-            jQuery('#open').parent().addClass('active');
-            jQuery('.breadcrumb').children().last().text("open source");
+            showPanel('#opensource','#open','open source');
         });
 
         jQuery('#str').click(function() {
-            hideAll();
-            jQuery('#stream').fadeIn('medium');
+            showPanel('#stream','#str','stream');
             jQuery('.carousel').show();
-            jQuery('#str').parent().addClass('active');
-            jQuery('.breadcrumb').children().last().text("stream");
         });
 
         jQuery('#write').click(function() {
-            hideAll();
-            jQuery('#writing').fadeIn('medium');
-            jQuery('#write').parent().addClass('active');
-            jQuery('.breadcrumb').children().last().text("writing");
+            showPanel('#writing','#write','writing');
         });
-
         hideAll();
         var url = jQuery(document.location).attr('href');
         var hash = url.substring(url.indexOf("#"));
         jQuery(hash).click();
-        
-        jQuery('.carousel').carousel({ interval: 6500});
+        jQuery('.carousel').carousel({ interval: 4000});
         jQuery('.carousel').hide();
 });
